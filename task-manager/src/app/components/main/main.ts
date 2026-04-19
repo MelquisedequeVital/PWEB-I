10169e4abde5c4bdf884cad00b88cea8dbfb0db2
Melquisedeque <melquisedequevital@gmail.com>
2026-04-19 13:12:38 -0300

    'dd drag and drop

--- task-manager/src/app/components/main/main.ts
+++ task-manager/src/app/components/main/main.ts
@@ -2,10 +2,11 @@ import { Component, inject, Signal, WritableSignal, signal, computed, Output, Ev
 import { TaskService } from '../../services/task.service';
 import { TaskModel } from '../../model/task.model';
 import { TaskComponent } from "../task/task";
+import { CdkDragDrop, DragDropModule, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-main',
-  imports: [TaskComponent],
+  imports: [TaskComponent, DragDropModule],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
@@ -20,8 +21,18 @@ export class MainComponent {
   protected doingTasks = computed(() => this.tasks().filter(t => t.status === 'doing'));
   protected doneTasks = computed(() => this.tasks().filter(t => t.status === 'done'));
 
-  editTaskMain(task: TaskModel){
+  editTaskMain(task: TaskModel) {
     this.editarTarefa.emit(task);
   }
 
+  protected onDrop(event: CdkDragDrop<TaskModel[]>) {
+    const taskMoved = event.item.data;
+    const newStatus = event.container.id as 'todo' | 'doing' | 'done';
+    const newTaskStatus = { ...taskMoved, status: newStatus }
+    const isMoving = true;
+    if (event.previousContainer !== event.container) {
+      this.taskService.updateTask(newTaskStatus, isMoving);
+    } 
+
+  }
 }
--- task-manager/src/app/services/task.service.ts
+++ task-manager/src/app/services/task.service.ts
@@ -11,21 +11,21 @@ export class TaskService {
   private tasksSignal = signal<TaskModel[]>([]);
   public readonly tasks = this.tasksSignal.asReadonly();
 
-  constructor(private storage: LocalStorageService){
+  constructor(private storage: LocalStorageService) {
     this.tasksSignal.set(this.storage.getItem() || [
-    { id: uid(), title: 'Ler capítulo 3 de Algoritmos', due: addDaysISO(2), level: 'high', desc: 'Priorizar exercícios 3.1-3.5', status: 'todo' },
-    { id: uid(), title: 'Resolver lista de TS', due: addDaysISO(5), level: 'medium', desc: 'Atenção a generics', status: 'doing' },
-    { id: uid(), title: 'Revisão rápida: HTML/CSS', due: addDaysISO(10), level: 'low', desc: '30 minutos', status: 'done' }]);
+      { id: uid(), title: 'Ler capítulo 3 de Algoritmos', due: addDaysISO(2), level: 'high', desc: 'Priorizar exercícios 3.1-3.5', status: 'todo' },
+      { id: uid(), title: 'Resolver lista de TS', due: addDaysISO(5), level: 'medium', desc: 'Atenção a generics', status: 'doing' },
+      { id: uid(), title: 'Revisão rápida: HTML/CSS', due: addDaysISO(10), level: 'low', desc: '30 minutos', status: 'done' }]);
   }
 
 
-  private saveTasks(){
+  private saveTasks() {
     this.storage.setItem(this.tasksSignal())
   }
 
 
-  addTask(task: Omit<TaskModel, "id">){
-    const newTask = {...task, id: this.uid()};
+  addTask(task: Omit<TaskModel, "id">) {
+    const newTask = { ...task, id: this.uid() };
     this.tasksSignal.update(allTasks => [...allTasks, newTask]);
     this.saveTasks();
   }
@@ -35,14 +35,18 @@ export class TaskService {
     this.saveTasks();
   }
 
-  updateTask(newTask: TaskModel){
-    this.tasksSignal.update(tasks =>
-      tasks.map(task => task.id === newTask.id ? newTask : task)
-    )
+  updateTask(newTask: TaskModel, isMoving: boolean = false) {
+    this.tasksSignal.update(tasks => {
+      if (isMoving) {
+        return [...tasks.filter(t => t.id !== newTask.id), newTask];
+      } else {
+        return tasks.map(t => t.id === newTask.id ? newTask : t);
+      }
+    });
     this.saveTasks()
   }
 
-  getTaskById(id: string){
+  getTaskById(id: string) {
     let taskFound = this.tasksSignal().find(task => task.id == id);
     return taskFound;
   }
